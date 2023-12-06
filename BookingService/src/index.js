c813a8c3b0e97a44da1e69cae943f064f0dcedc7
Valeria Dubina <valeria.dubina@isa.utm.md>
2023-12-06 21:26:01 +0200

    configure elk stack

--- BookingService/src/index.js
+++ BookingService/src/index.js
@@ -1,5 +1,13 @@
 import dotenv from "dotenv";
 import protoServer from "./proto/BookingServer.js";
-
+import {Client} from "@elastic/elasticsearch";
 dotenv.config();
 protoServer;
+
+const client = new Client({node: 'http://elasticsearch:9200'});
+
+await client.index({
+    body: { message: 'Booking server started!' },
+    index: 'booking'
+});
+
--- ScootersService/src/index.js
+++ ScootersService/src/index.js
@@ -1,5 +1,13 @@
 import dotenv from "dotenv";
 import grpcServer from "./proto/ScooterServer.js";
+import {Client} from "@elastic/elasticsearch";
 
 dotenv.config();
 grpcServer;
+
+const client = new Client({node: 'http://elasticsearch:9200'});
+
+await client.index({
+    body: { message: 'Scooter server started!' },
+    index: 'scooter'
+});
